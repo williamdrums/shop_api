1b2d16ee3982839e4dfb7f7076101344c3b001df
williamdrums <williandrums@gmail.com>
2020-12-12 17:23:03 -0400

    adicionando crud de produto

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,121 @@
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("product")]
+    public class ProductController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context
+            .Products
+            .Include(x => x.Category)
+            .AsNoTracking().ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("id:int")]
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
+        {
+            var product = await context
+            .Products
+            .Include(x => x.Category)
+            .AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return product;
+
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
+        {
+            var products = await context
+            .Products
+            .Include(x => x.Category)
+            .AsNoTracking()
+            .Where(x => x.CategoryId == id)
+            .ToListAsync();
+            return products;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post(
+            [FromServices] DataContext context,
+            [FromBody] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            else
+            {
+                return BadRequest(model);
+            }
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> Put(int id,
+        [FromBody] Product model,
+        [FromServices] DataContext context)
+        {
+            if (id != model.Id)
+                return NotFound(new { message = "Produto não encontrado!" });
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Entry<Product>(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();//persiste os dados no banco
+                return model;
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest(new { message = "Este registro já foi atualizado" });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possivel atualizar a categoria" });
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> Delete(int id, [FromServices] DataContext context)
+        {
+            var product = await context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound(new { message = "Categoria não encontrada" });
+            }
+
+            try
+            {
+                context.Products.Remove(product);
+                await context.SaveChangesAsync();
+                return Ok(new { message = "Produto removido com sucesso!" });
+            }
+            catch (Exception)
+            {
+                return NotFound(new { message = "Não foi possivel remover o produto" });
+            }
+
+
+        }
+    }
+}
